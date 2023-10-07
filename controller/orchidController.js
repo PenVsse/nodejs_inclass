2eb0136e3ae0762a2298a227685b502568b7e0be
phuvs <vusiphu20042000@gmail.com>
2023-10-07 18:40:00 +0700

    up

--- controller/orchidController.js
+++ controller/orchidController.js
@@ -1,11 +1,9 @@
 const Orchids = require("../model/orchid");
 
-class orchidController {
-  index(req, res, nexy) {
-    Orchids.find({}).then((data) => {
-      res.render("orchid", { title: "Lit of Orchids", orchidData: data });
-    });
-  }
-}
+const index = (req, res, nexy) => {
+  Orchids.find({}).then((data) => {
+    res.render("orchid", { title: "Lit of Orchids", orchidData: data });
+  });
+};
 
-module.exports = new orchidController();
+module.exports = { index };
